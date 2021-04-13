1be8ff87298dd9a58e61d0b14ec159e9d0df9dcb
Jakub <rutrle@seznam.cz>
2021-04-14 00:20:31 +0200

    quick testing and fixes

--- linear_search.py
+++ linear_search.py
@@ -1,12 +1,15 @@
-def linear_search(list, target):
+def linear_search(search_list, target):
     """
     returns position of target if found, if not return None
     """
 
-    for index, value in list:
+    for index, value in enumerate(search_list):
         if value == target:
             return index
 
-    return None        
-        
+    return None
 
+
+my_list = ['a', 'b', 'c', 'd', 'e']
+
+print(linear_search(my_list, 'd'))
