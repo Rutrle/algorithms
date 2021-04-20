d7fec16501c45907898e6b9163a00d0234749531
Jakub <rutrle@seznam.cz>
2021-04-20 22:43:09 +0200

    insert method added in linked list

--- linked_list.py
+++ linked_list.py
@@ -64,6 +64,31 @@ class LinkedList:
                 current = current.next_node
         return None
 
+    def insert(self, data, index):
+        """
+        Inserts a new node containing data at index position
+        Insertion takes O(1), but finding the node takes O(n) time
+
+        therefore overall timeis O(n)
+        """
+        if index == 0:
+            self.add(data)
+
+        elif index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node_obj = current
+            next_node_obj = current.next_node
+
+            prev_node_obj.next_node = new
+            new.next_node = next_node_obj
+
     def __repr__(self):
         """
         Return a string representation of the list.
