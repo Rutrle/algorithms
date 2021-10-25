419a69a33544bad89356523b912805e2bdbfcec5
Jakub <rutrle@seznam.cz>
2021-10-25 21:37:59 +0200

    bugfix + wrapper func

--- recursions/decimal_to_binary.py
+++ recursions/decimal_to_binary.py
@@ -2,15 +2,19 @@ def decimal_to_binary(number):
     if number == 0:
         return ''
     else:
-        return str(number % 2) + decimal_to_binary(number//2)
+        return decimal_to_binary(number//2) + str(number % 2)
 
 
 def decimal_to_binary2(number, _result=""):
     if number == 0:
         return _result
     else:
-        _result = _result + str(number % 2)
+        _result = str(number % 2) + _result
         return decimal_to_binary2(number//2, _result)
+
+
+def decimal_to_binary2_wrap(number):
+    return decimal_to_binary2(number)
 
 
 print(decimal_to_binary(1))
@@ -19,3 +23,4 @@ print(decimal_to_binary(233))
 
 print(decimal_to_binary2(1))
 print(decimal_to_binary2(233))
+print(decimal_to_binary2_wrap(10))
